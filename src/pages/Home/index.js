517779171e3a13f7c5f1079f43d5ef4a69435ea4
Gustavo Henrique Michels <ghmichels@gmail.com>
2019-08-13 10:35:09 -0300

    styles done

---
+++ src/pages/Home/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-proof-masculino/88/D22-0359-088/D22-0359-088_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Olympikus Proof Masculino - Marinho e Royal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-proof-masculino/88/D22-0359-088/D22-0359-088_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Olympikus Proof Masculino - Marinho e Royal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-proof-masculino/88/D22-0359-088/D22-0359-088_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Olympikus Proof Masculino - Marinho e Royal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-proof-masculino/88/D22-0359-088/D22-0359-088_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Olympikus Proof Masculino - Marinho e Royal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-proof-masculino/88/D22-0359-088/D22-0359-088_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Olympikus Proof Masculino - Marinho e Royal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-proof-masculino/88/D22-0359-088/D22-0359-088_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Olympikus Proof Masculino - Marinho e Royal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
